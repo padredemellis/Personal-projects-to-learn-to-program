327a83b2168585c2c761a97f17c5c231b938691e
padredemellis <10656@holbertonstudents.com>
2025-12-17 23:38:22 -0300

    validacion

--- TTT(THE_TASKS_TROPHY)/ttt/auth.py
+++ TTT(THE_TASKS_TROPHY)/ttt/auth.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, render_template, request, url_for, redirect, flash
+    Blueprint, render_template, request, url_for, redirect, flash, session, g
     )
 
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -31,6 +31,38 @@ def register():
         flash(error)
     return render_template("auth/register.html")
 
-@bp.route('/login')
+@bp.route('/login', methods = ('GET', 'POST'))
 def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        
+
+        error = None
+        #Validar datos
+
+        user = User.query.filter_by(username = username).first()
+
+        if user == None:
+            error = 'Nombre de usuario incorrecto'
+        elif not check_password_hash(user.password, password):
+            error = 'Contraseña incorrecta'
+
+        # Iniciar sesion
+        if error is None:
+            session.clear()
+            session['user_id'] = user.id
+            return redirect(url_for('ttt.index'))
+        
+        flash(error)
     return render_template("auth/login.html")
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id == None:
+        g.user = None
+    else:
+        g.user = User.query.get_or_404(user_id)
--- TTT(THE_TASKS_TROPHY)/ttt/main.py
+++ TTT(THE_TASKS_TROPHY)/ttt/main.py
@@ -1,11 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint('The_task_trophy', __name__, url_prefix = "/The_task_trophy")
 
 @bp.route('/list')
 def index():
-    return "Lists of task"
-
+    return render_template('ttt/index.html')
 @bp.route('/create')
 def create():
     return "Crear una tarea"
